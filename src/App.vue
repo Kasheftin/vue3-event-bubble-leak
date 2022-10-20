8dd63a032e219e302d7c37e359bb4e84b5bc87ba
Kasheftin <Kasheftin@gmail.com>
2022-10-20 15:40:26 +0300

    Initial commit: event bubble leak demo

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <Test1 @change="changeHandler" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Test1 from './Test1.vue'
+const changeHandler = () => {
+  console.log('Change event triggered in App.vue')
+}
+</script>
+
+<style>
+.wrapper {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
---
+++ src/Test1.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <draggable
+      v-model="items"
+      item-key="id"
+    >
+      <template #item="{ element }">
+        <div class="item">{{ element.name }}</div>
+      </template>
+    </draggable>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import draggable from 'vuedraggable'
+
+export default defineComponent({
+  components: {
+    draggable
+  },
+  emits: ['run'],
+  data() {
+    return {
+      items: [
+        {id: 1, name: 'first'}, 
+        {id: 2, name: 'second'},
+        {id: 3, name: 'third'}
+      ]
+    }
+  }
+})
+</script>
+
+<style>
+.container {
+  width: 300px;
+  border: 1px solid #000;
+}
+.item {
+  padding: 10px;
+  margin: 10px;
+  border: 1px solid #000;
+}
+</style>
